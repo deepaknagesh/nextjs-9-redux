346fdaf6d43aae741e7e9a8eb476900896876123
Deepak N <deepaknagesh05@gmail.com>
2020-04-13 14:23:25 +0530

    Updates

---
+++ _next/static/mmeHRY08q0HW_j22d9yPt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/2":[a],"/index":[a]}}("static\u002Fchunks\u002F49382f716293dea885399206ad3e79fcb2a8b3b0.152c8d9cd1182b086cde.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
